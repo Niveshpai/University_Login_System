53e51e6f4d274403c9903ffdaad4355e607770dd
Nivesh <59831140+Niveshpai@users.noreply.github.com>
2020-11-15 22:44:16 +0530

    For Overall Information
    This is used to :
    1. Store the Student' and Professor's details.
    2. Store the information about the number of courses available.

---
+++ objects.py
@@ -0,0 +1,29 @@
+from professor import Professor
+from course_uni import Course
+from student import Student
+
+professor1 = Professor(1, 'John', 'Doe')
+professor2 = Professor(2, 'Robert', 'Fischer')
+professor3 = Professor(3, 'Angela')
+professor4 = Professor(4, 'Jimmy')
+professor5 = Professor(5, 'Ravi')
+professor6 = Professor(6, 'Joe')
+
+English = Course(1, 'English')
+Artifical_Intelligence = Course(2, 'Artifical Intelligence')
+Machine_Learning = Course(3, 'Machine Learning')
+Ethical_Hacking = Course(4, 'Ethical Hacking')
+Javascript = Course(5, 'Javascript')
+Physics = Course(6, 'Physics')
+Calculus = Course(7, 'Calculus')
+
+student_1 = Student(100, 'Cynthia', 'Brook')
+student_2 = Student(101, 'Keith', 'Brewer')
+student_3 = Student(102, 'Mehmet', 'Faulkner')
+student_4 = Student(103, 'Anabel', 'Molloy')
+student_5 = Student(104, 'Macy', 'Clark')
+student_6 = Student(105, 'Aisha', 'Berger')
+student_7 = Student(106, 'Niall', 'Farrington')
+student_8 = Student(107, 'Beulah', 'Esparza')
+student_9 = Student(108, 'Dorsey', 'Montgomery')
+student_10 = Student(109, 'Felicia', 'Ward')
